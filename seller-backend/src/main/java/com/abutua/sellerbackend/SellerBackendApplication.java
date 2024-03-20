f8d7f3583091b1d5cb71d4b7ed8cbd0cea8925af
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-03-20 12:55:55 -0300

    closes #1 - Primeiro commit e criando o monorepo

---
+++ seller-backend/src/main/java/com/abutua/sellerbackend/SellerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.abutua.sellerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerBackendApplication.class, args);
+	}
+
+}
---
+++ seller-backend/src/test/java/com/abutua/sellerbackend/SellerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abutua.sellerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
